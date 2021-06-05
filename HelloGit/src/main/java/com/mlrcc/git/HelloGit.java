91ea79b816b8951e160c8b7decd2fd2844ddb55a
xianga <ang.k@qq.com>
2021-06-05 20:25:12 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/mlrcc/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.mlrcc.git;
+
+/**
+ * @author xiangang
+ * @create 2021-06-05 20:14
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+
+}
